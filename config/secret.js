49ebb5df4c6997ab27e5ae0c07dd113aabc926a1
noafalik <108616439+noafalik@users.noreply.github.com>
2023-05-15 13:48:02 +0300

    Add files via upload

---
+++ config/secret.js
@@ -0,0 +1,12 @@
+// קובץ שמכיל את כל המשתנים הסודיים
+// dotEnv -> ספרייה שיודעת לקרוא משתנים מקובץ ENV
+// ENV
+require("dotenv").config();
+// console.log(process.env.DBUSER)
+
+exports.config = {
+  mongoUser:process.env.DBUSER,
+  mongoPass:process.env.DBPASS,
+  tokenSecret:process.env.TOKENSECRET
+  
+}
---
+++ models/commentModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+let schema = new mongoose.Schema({
+    user_id: String,
+    content: String,
+    date_created: {
+        type: Date, default: Date.now
+    },
+    stars: Number,
+    likes: {
+        type: Number, default: 0
+    }
+})
+exports.CommentModel = mongoose.model("comments", schema)
+
+exports.validateComment = (_reqBody) => {
+    let joiSchema = Joi.object({
+        content: Joi.string().min(2).max(1200).allow(null, ""),
+        stars: Joi.number().min(1).max(5).required(),
+    })
+    return joiSchema.validate(_reqBody)
+}
