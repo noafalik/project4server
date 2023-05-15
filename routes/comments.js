26b9d4c4dac018f3f1d5dd14bcd6dd5e694c83cc
noafalik <108616439+noafalik@users.noreply.github.com>
2023-05-15 13:48:02 +0300

    Add files via upload

---
+++ routes/comments.js
@@ -0,0 +1,113 @@
+const express = require("express");
+const router = express.Router();
+const { CommentModel, validateComment } = require("../models/commentModel");
+const { auth } = require("../middlewares/auth");
+
+router.get("/", async (req, res) => {
+  const perPage = req.query.perPage || 4;
+  const page = req.query.page - 1 || 0;
+  const user_id = req.query.user_id;
+
+  try {
+    let filterFind = {}
+    // בודק אם קיבלנו קווארי של יוזר ואם כן משנה את הפליטר של הפיינד
+    // למציאת פריטים שקשורים ליוזר
+    if (user_id) {
+      filterFind = { user_id }
+    }
+    let data = await CommentModel.find(filterFind)
+      .limit(perPage)
+      .skip(page * perPage)
+    res.json(data);
+  }
+  catch (err) {
+    console.log(err);
+    res.status(502).json({ err })
+  }
+})
+
+router.post("/", auth, async (req, res) => {
+  let validBody = validateComment(req.body);
+  if (validBody.error) {
+    return res.status(400).json(validBody.error.details);
+  }
+  try {
+    let comment = new CommentModel(req.body);
+    comment.user_id = req.tokenData._id
+    await comment.save();
+    res.json(comment)
+  }
+  catch (err) {
+    console.log(err);
+    res.status(502).json({ err })
+  }
+})
+
+router.patch("/inc/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const comment = await CommentModel.findById(id);
+
+    if (!comment) {
+      return res.status(404).json({ error: "Comment not found" });
+    }
+
+    // Increment the likes count by 1
+    comment.likes += 1;
+
+    // Save the updated comment
+    const updatedComment = await comment.save();
+
+    res.json(updatedComment);
+  } catch (err) {
+    console.log(err);
+    res.status(502).json({ err });
+  }
+});
+
+
+router.patch("/dec/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    
+    const comment = await CommentModel.findById(id);
+
+    if (!comment) {
+      return res.status(404).json({ error: "Comment not found" });
+    }
+
+    // Decrement the likes count by 1
+    comment.likes -= 1;
+
+    // Save the updated comment
+    const updatedComment = await comment.save();
+
+    res.json(updatedComment);
+  } catch (err) {
+    console.log(err);
+    res.status(502).json({ err });
+  }
+});
+
+
+router.delete("/delete/:id", auth, async (req, res) => {
+  try {
+    let id = req.params.id;
+    let data;
+    // נותן אפשרות לאדמין למחוק את כל הרשומות
+    if (req.tokenData.role == "admin") {
+      data = await CommentModel.deleteOne({ _id: id});
+    }
+    else{
+      data = await CommentModel.deleteOne({ _id: id, user_id: req.tokenData._id });
+    }
+    res.json(data)
+  }
+  catch (err) {
+    console.log(err);
+    res.status(502).json({ err })
+  }
+})
+
+module.exports = router;
---
+++ routes/configRoutes.js
@@ -0,0 +1,26 @@
+const indexR = require("./index");
+const usersR = require("./users");
+const jobsR = require("./jobs");
+const contendersR = require("./contenders");
+const companiesR = require("./companies");
+const commentsR = require("./comments");
+
+
+
+
+
+exports.routesInit = (app) => {
+  app.use("/",indexR);
+  app.use("/users",usersR);
+  app.use("/jobs",jobsR);
+  app.use("/contenders",contendersR);
+  app.use("/companies",companiesR);
+  app.use("/comments",commentsR);
+
+
+
+  //show 404 routes
+  app.use("/*",(req,res) =>{
+    res.status(404).json({msg:"Endpoint/page not found, 404"})
+  })
+}
