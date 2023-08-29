38e0788fc007fec927af7b112433d076388551ba
noafalik <108616439+noafalik@users.noreply.github.com>
2023-08-29 09:45:25 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -117,7 +117,7 @@ router.post("/login", async (req, res) => {
     let token = createToken(user._id, user.role)
     // {token} -> {token:token } אם השם של המאפיין ומשתנה/פרמטר זהה אין צורך בנקודתיים
     // shotcut prop value
-    res.cookie('token', token, { httpOnly: true, sameSite: "lax" });
+    res.cookie('token', token, { httpOnly: false , sameSite: "none",secure:true });
 
     return res.status(200).json({ message: "Logged in", login: true });
   }
